61a6bfca90584549838279b8cd8595c6bb3a58fa
Saniya-innovates <146875076+saniya-khan11@users.noreply.github.com>
2025-02-20 19:43:23 +0530

    Creating 2D Arrays in Java.

---
+++ Array2d.java
@@ -0,0 +1,12 @@
+class Array2d{
+	public static void main(String args[ ]){
+		 
+		int myarray[][]={{1,2,3},{4,5,6}};
+		for (int i=0;i<2;i++){
+			for(int j=0;j<3;j++)
+				System.out.print(myarray[i][j] + " ");
+			System.out.println();
+			}
+	}
+}
+		
